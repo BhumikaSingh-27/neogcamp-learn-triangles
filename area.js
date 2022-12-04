ce3fc663a151855a8d3255c31957a670d30ec146
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2022-12-04 15:09:56 +0530

    feat: updated app.js
    added validation check for negative inputs

--- area.js
+++ area.js
@@ -3,21 +3,19 @@ const calculateAreaBtn = document.querySelector("#calculateArea");
 
 const outputArea = document.querySelector("#output");
 
-
-
-function calculateArea(){
-
-    if ((sideInputs[0].value == "") || (sideInputs[1].value == "")){
-        console.log("there");
-        
-        outputArea.innerText = "Invalid Input!"
-    }  else {
-        
-    const area = (Number(sideInputs[0].value)*Number(sideInputs[1].value))/2
-    outputArea.innerText = "The area of triangle is "+ area + " cm²";
+function calculateArea() {
+  if (
+    sideInputs[0].value == "" ||
+    sideInputs[1].value == "" ||
+    sideInputs[0].value < 0 ||
+    sideInputs[1].value < 0
+  ) {
+    outputArea.innerText = "Invalid Input!";
+  } else {
+    const area =
+      (Number(sideInputs[0].value) * Number(sideInputs[1].value)) / 2;
+    outputArea.innerText = "The area of triangle is " + area + " cm²";
+  }
 }
 
-} 
-    
-
-calculateAreaBtn.addEventListener("click", calculateArea)
+calculateAreaBtn.addEventListener("click", calculateArea);
