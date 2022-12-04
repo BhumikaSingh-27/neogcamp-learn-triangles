aa86ff65f2316a764502fb4746ba51403e826b21
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2022-12-04 15:53:06 +0530

    feat: updated app.js
    added invalid input check for hypotenuse

--- calculateHypo.js
+++ calculateHypo.js
@@ -3,17 +3,29 @@ const calculateHypotenuseBtn = document.querySelector("#calulate-hypo-btn");
 
 const outputHypotenuse = document.querySelector("#output-hypo");
 
-
-function sumOfSquares(a,b){
-   const sum = a*a + b*b;
-    return sum;
-    // console.log(sum)
+function sumOfSquares(a, b) {
+  const sum = a * a + b * b;
+  return sum;
+  // console.log(sum)
 }
 
-function calculateHypotenuse(){
-   const sumOfSquareSides = sumOfSquares(Number(sideInputs[0].value) , Number(sideInputs[1].value));
+function calculateHypotenuse() {
+  if (
+    sideInputs[0].value == "" ||
+    sideInputs[1].value == "" ||
+    sideInputs[0].value < 0 ||
+    sideInputs[1].value < 0
+  ) {
+    outputHypotenuse.innerText = "Invalid Input!";
+  } else {
+    const sumOfSquareSides = sumOfSquares(
+      Number(sideInputs[0].value),
+      Number(sideInputs[1].value)
+    );
 
-   const lengthOfHypotenuse = Math.sqrt(sumOfSquareSides)
-    outputHypotenuse.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse;
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquareSides);
+    outputHypotenuse.innerText =
+      "The length of Hypotenuse is " + lengthOfHypotenuse;
+  }
 }
-calculateHypotenuseBtn.addEventListener("click", calculateHypotenuse)
+calculateHypotenuseBtn.addEventListener("click", calculateHypotenuse);
